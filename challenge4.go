af792929ff080cd98f26e1144dc457225b974773
Erik J. Sturcke <esturcke@cisco.com>
2018-12-15 14:16:24 -0500

    Solve challenge 4

---
+++ challenge4.go
@@ -0,0 +1,72 @@
+package cryptopals
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/esturcke/cryptopals-golang/english"
+)
+
+/*
+
+# Detect single-character XOR
+
+The hex encoded string:
+
+
+One of the 60-character strings in this [file](https://cryptopals.com/static/challenge-data/4.txt)
+has been encrypted by single-character XOR.
+
+Find it.
+```
+
+*/
+func solve4() string {
+	var topScore float64
+	var pt string
+	for _, ct := range getCts() {
+		score, guess := decodeByteXor(ct)
+		if score > topScore {
+			topScore, pt = score, guess
+		}
+	}
+	return pt
+}
+
+func decodeByteXor(ct []byte) (float64, string) {
+	var topScore float64
+	var pt []byte
+	key := make([]byte, len(ct))
+	for b := 0; b <= 255; b++ {
+		for i := range ct {
+			key[i] = byte(b)
+		}
+		guess := xor(ct, key)
+		score := english.LikeEnglish(guess)
+		if score > topScore {
+			pt = guess
+			topScore = score
+		}
+	}
+	return topScore, string(pt)
+}
+
+func getCts() [][]byte {
+	file, err := os.Open("data/4.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var cts [][]byte
+	for scanner.Scan() {
+		cts = append(cts, fromHex(scanner.Text()))
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return cts
+}
--- cryptopals.go
+++ cryptopals.go
@@ -12,4 +12,5 @@ var Challenges = []Challenge{
 	{1, solve1, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
 	{2, solve2, "746865206b696420646f6e277420706c6179"},
 	{3, solve3, "Cooking MC's like a pound of bacon"},
+	{4, solve4, "Now that the party is jumping\n"},
 }
