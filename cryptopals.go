6cc689c9fc7b7ef6e37dbbf4b04344536e37b185
Erik J. Sturcke <esturcke@cisco.com>
2018-12-15 15:08:00 -0500

    Solve challenge 5

--- cryptopals.go
+++ cryptopals.go
@@ -13,4 +13,5 @@ var Challenges = []Challenge{
 	{2, solve2, "746865206b696420646f6e277420706c6179"},
 	{3, solve3, "Cooking MC's like a pound of bacon"},
 	{4, solve4, "Now that the party is jumping\n"},
+	{5, solve5, "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"},
 }
